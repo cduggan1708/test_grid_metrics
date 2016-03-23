4b1b8e0bfde234acdcc3bc3c527eb6b463abbfc2
cduggan1708 <cduggan1708@gmail.com>
2016-03-23 15:32:20 -0400

    updated column_letter dictionary and made changes to template files

--- test_grid_metrics.py
+++ test_grid_metrics.py
@@ -6,7 +6,9 @@ from MemberMetricData import MemberMetricData
 
 column_letter = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J',
                  11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S',
-                 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z', 27: 'AA', 28: 'AB', #etc
+                 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z', 27: 'AA', 28: 'AB', 
+                 29: 'AC', 30: 'AD', 31: 'AE', 32: 'AF', 33: 'AG', 34: 'AH', 35: 'AI', 36: 'AJ', 37: 'AK', 
+                 38: 'AL', 39: 'AM', 40: 'AN', 41: 'AO', 42: 'AP', 43: 'AQ', 44: 'AR', 45: 'AS', #etc
                  # specific values that are hardcoded (based on template)
                  'MemberID': 'B'}
 
@@ -98,6 +100,7 @@ def getRelevantCellsInTestGrid(test_grid_file):
     last_column = grid_sheet.max_column
     first_metric_column = 0
     j = 0
+
     for column in range(1, last_column):
         value = grid_sheet[column_letter[column] + str(first_row)].value
         j += 1
